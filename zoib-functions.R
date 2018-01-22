782526293b64206e3f1a28d4629221b53a502994
Sean Anderson <sean@seananderson.ca>
2018-01-22 10:25:41 -0800

    Expand analyses

--- zoib-functions.R
+++ zoib-functions.R
@@ -24,6 +24,12 @@ get_dat <- function(response, main_predictor,
 }
 
 prep_stan_dat <- function(d, predictors = "xscaled") {
+
+  # in case some missing now (e.g. cross-validation):
+  d <- d %>% mutate(
+      group = as.factor(as.character(group)),
+      group_id = as.integer(group)) %>%
+    arrange(group_id, x)
   dp <- filter(d, !is.na(yp))
 
   N0 <- nrow(d)
@@ -50,7 +56,7 @@ prep_stan_dat <- function(d, predictors = "xscaled") {
     y1_i = d$y1,
     yp_i = dp$y,
 
-    Ng = length(unique(d$group)),
+    Ng = length(unique(as.character(d$group))),
     group_id0 = d$group_id,
     group_id1 = d$group_id,
     group_idp = dp$group_id)
@@ -72,7 +78,8 @@ fit_model <- function(d, model, predictors = "xscaled", iter = 800L, chains = 4L
   list(model = m, data = d, f = prep$f, stan_dat = prep$stan_dat)
 }
 
-make_predictions <- function(d, f, model, Npred = 200L, use_new_data = TRUE) {
+make_predictions <- function(d, f, model, Npred = 200L, use_new_data = TRUE,
+  re = TRUE, sd_x = NULL, mean_x = NULL) {
 
   if (use_new_data) {
     d_pred <- expand.grid(
@@ -92,58 +99,61 @@ make_predictions <- function(d, f, model, Npred = 200L, use_new_data = TRUE) {
   # plot(d_pred$xscaled, apply(pred_0, 1, median), ylim = c(0, 1))
   pred_1 <- plogis(mm_pred %*% t(e$b1_j))
   # plot(d_pred$xscaled, apply(pred_1, 1, median), ylim = c(0, 1))
-
-  pred_p_re <- mm_pred %*% t(e$bp_j)
-  pred_p_re <- apply(pred_p_re, 1, median)
-  zp_g <- apply(e$zp_g, 2, median)
-  # zp_g <- tibble(zp_g = zp_g, group_id = seq_along(zp_g))
-
-  pred_0_re <- mm_pred %*% t(e$b0_j)
-  pred_0_re <- apply(pred_0_re, 1, median)
-  z0_g <- apply(e$z0_g, 2, median)
-  # z0_g <- tibble(z0_g = z0_g, group_id = seq_along(z0_g))
-
-  pred_1_re <- mm_pred %*% t(e$b1_j)
-  pred_1_re <- apply(pred_1_re, 1, median)
-  z1_g <- apply(e$z1_g, 2, median)
-  # z1_g <- tibble(z1_g = z1_g, group_id = seq_along(z1_g))
-  #
-  # d_pred <- inner_join(d_pred, zp_g, by = "group_id")
-  # d_pred <- inner_join(d_pred, z0_g, by = "group_id")
-  # d_pred <- inner_join(d_pred, z1_g, by = "group_id")
-  #
-  # d_pred <- mutate(d_pred,
-  #   pred_p_re = pred_p_re + zp_g,
-  #   pred_0_re = pred_0_re + z0_g,
-  #   pred_1_re = pred_1_re + z1_g
-  # )
-
-  d_pred$pred_p_re <- plogis(pred_p_re + rep(zp_g, each = Npred))
-  d_pred$pred_0_re <- plogis(pred_0_re + rep(z0_g, each = Npred))
-  d_pred$pred_1_re <- plogis(pred_1_re + rep(z1_g, each = Npred))
-
   y_pred <- (1 - pred_0) * (pred_1 + (1 - pred_1) * pred_p)
-  d_pred$y_pred_re <- (1 - d_pred$pred_0_re) *
-    (d_pred$pred_1_re + (1 - d_pred$pred_1_re) * d_pred$pred_p_re)
+  d_pred$y_pred <- y_pred
+  if (re) {
+    pred_p_re <- mm_pred %*% t(e$bp_j)
+    pred_p_re <- apply(pred_p_re, 1, median)
+    zp_g <- apply(e$zp_g, 2, median)
+    # zp_g <- tibble(zp_g = zp_g, group_id = seq_along(zp_g))
+
+    pred_0_re <- mm_pred %*% t(e$b0_j)
+    pred_0_re <- apply(pred_0_re, 1, median)
+    z0_g <- apply(e$z0_g, 2, median)
+    # z0_g <- tibble(z0_g = z0_g, group_id = seq_along(z0_g))
+
+    pred_1_re <- mm_pred %*% t(e$b1_j)
+    pred_1_re <- apply(pred_1_re, 1, median)
+    z1_g <- apply(e$z1_g, 2, median)
+    # z1_g <- tibble(z1_g = z1_g, group_id = seq_along(z1_g))
+    #
+    # d_pred <- inner_join(d_pred, zp_g, by = "group_id")
+    # d_pred <- inner_join(d_pred, z0_g, by = "group_id")
+    # d_pred <- inner_join(d_pred, z1_g, by = "group_id")
+    #
+    # d_pred <- mutate(d_pred,
+    #   pred_p_re = pred_p_re + zp_g,
+    #   pred_0_re = pred_0_re + z0_g,
+    #   pred_1_re = pred_1_re + z1_g
+    # )
+
+    d_pred$pred_p_re <- plogis(pred_p_re + rep(zp_g, each = Npred))
+    d_pred$pred_0_re <- plogis(pred_0_re + rep(z0_g, each = Npred))
+    d_pred$pred_1_re <- plogis(pred_1_re + rep(z1_g, each = Npred))
+    d_pred$y_pred_re <- (1 - d_pred$pred_0_re) *
+      (d_pred$pred_1_re + (1 - d_pred$pred_1_re) * d_pred$pred_p_re)
+  }
 
   pred_df <- data.frame(
     xscaled = d_pred$xscaled,
     group = d_pred$group_id,
     est = apply(y_pred, 1, quantile, probs = 0.5),
     lwr = apply(y_pred, 1, quantile, probs = 0.025),
-    upr = apply(y_pred, 1, quantile, probs = 0.975),
-    est_re = d_pred$y_pred_re,
-    x = d_pred$xscaled * 2 * sd(d$x) + mean(d$x)
-  )
+    upr = apply(y_pred, 1, quantile, probs = 0.975))
+
+  if (re)
+    pred_df$est_re = d_pred$y_pred_re
+
+  if (is.null(sd_x))
+    pred_df$x <- d_pred$xscaled * 2 * sd(d$x) + mean(d$x)
+
   pred_df
 }
 
-make_pred_plot <- function(pred_df, raw_data) {
-  ggplot(pred_df, aes(x, est, ymin = lwr, ymax = upr)) +
+make_pred_plot <- function(pred_df, raw_data, re = TRUE) {
+  g <- ggplot(pred_df, aes(x, est, ymin = lwr, ymax = upr)) +
     geom_point(data = d, aes(x, y, colour = group),
       inherit.aes = FALSE, alpha = 0.6) +
-    geom_line(aes(y = est_re, group = as.factor(group)),
-      col = "grey50", lty = 1, lwd = 0.2) +
     geom_line(lwd = 1.5) +
     geom_ribbon(alpha = 0.5, fill = "grey20") +
     ylim(0, 1) +
@@ -151,6 +161,10 @@ make_pred_plot <- function(pred_df, raw_data) {
     ylab("Proportion burned") +
     xlab("Predictor value") +
     labs(colour = "Fire location")
+  if (re)
+    g <- g + geom_line(aes(y = est_re, group = as.factor(group)),
+      col = "grey50", lty = 1, lwd = 0.2)
+  g
 }
 
 # https://gist.github.com/traversc/1446ebe1dcc2d84bccdca781d4f1fa2a
@@ -185,8 +199,7 @@ make_predictions_dat <- function(model, d, f) {
 }
 
 make_roc <- function(d, predictions) {
-  thresh <- seq(0.05, 0.95, length.out = 9)
-
+  thresh <- seq(0.2, 0.8, length.out = 8)
   auc_thresh <- function(thresh = 0.5) {
     d$outcome <- ifelse(d$y > thresh, 1, 0)
     fastAUC(class = d$outcome, probs = predictions)
