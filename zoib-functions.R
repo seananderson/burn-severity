da5fd4da8ba88ce3ed67de3bdc0ca28ddd04faf0
Sean Anderson <sean@seananderson.ca>
2019-02-01 20:49:58 -0500

    Style and clean

--- zoib-functions.R
+++ zoib-functions.R
@@ -1,20 +1,22 @@
 get_dat <- function(response, main_predictor,
-  file = "RockiesBurnSev_clean_20180216.csv",
-  scale_other_predictors = TRUE,
-  max_predictor = 2500) {
-
+                    file = "RockiesBurnSev_clean_20180216.csv",
+                    scale_other_predictors = TRUE,
+                    max_predictor = 2500) {
   response <- enquo(response)
   main_predictor <- enquo(main_predictor)
 
   d <- suppressMessages(read_csv(file))
 
-  if ("pre_NBR" %in% names(d)) # col name was changed
+  if ("pre_NBR" %in% names(d)) { # col name was changed
     d <- rename(d, preNBR = pre_NBR)
+  }
 
-  d <- select(d, !!response, !!main_predictor, FIRE,
+  d <- select(
+    d, !!response, !!main_predictor, FIRE,
     lat, HeatLoad, slope, preNBR, # Q2
     slope, HeatLoad, # Q3
-    QMD_LAF, QMD_all, ba_ha, stems_ha, RedGrayGreenStage.BA.p) %>%
+    QMD_LAF, QMD_all, ba_ha, stems_ha, RedGrayGreenStage.BA.p
+  ) %>%
     filter(!is.na(!!response), !is.na(!!main_predictor)) %>%
     rename(y = !!response, x = !!main_predictor, group = FIRE) %>%
     filter(x <= max_predictor) %>%
@@ -26,17 +28,22 @@ get_dat <- function(response, main_predictor,
       xraw_sd = sd(x),
       xscaled = arm::rescale(x),
       group = as.factor(tolower(group)),
-      group_id = as.integer(group)) %>%
+      group_id = as.integer(group)
+    ) %>%
     arrange(group_id, x)
 
   names(d) <- tolower(names(d))
   names(d) <- gsub("\\.", "_", names(d))
 
   if (scale_other_predictors) {
-    d <- dplyr::mutate_at(d,
-      c("lat",  "heatload",  "slope", "prenbr", "qmd_laf", "qmd_all",
-        "ba_ha", "stems_ha", "redgraygreenstage_ba_p"),
-      function(x) arm::rescale(x))
+    d <- dplyr::mutate_at(
+      d,
+      c(
+        "lat", "heatload", "slope", "prenbr", "qmd_laf", "qmd_all",
+        "ba_ha", "stems_ha", "redgraygreenstage_ba_p"
+      ),
+      function(x) arm::rescale(x)
+    )
   }
   d
 }
@@ -44,9 +51,11 @@ get_dat <- function(response, main_predictor,
 prep_stan_dat_oib <- function(d, predictors = "xscaled") {
 
   # in case some missing now (e.g. cross-validation):
-  d <- d %>% mutate(
-    group = as.factor(as.character(group)),
-    group_id = as.integer(group)) %>%
+  d <- d %>%
+    mutate(
+      group = as.factor(as.character(group)),
+      group_id = as.integer(group)
+    ) %>%
     arrange(group_id, x)
   dp <- filter(d, !is.na(yp))
   d1 <- filter(d, !is.na(y1))
@@ -78,7 +87,8 @@ prep_stan_dat_oib <- function(d, predictors = "xscaled") {
     Ng = length(unique(as.character(d$group))),
     # group_id0 = d$group_id,
     group_id1 = d1$group_id,
-    group_idp = dp$group_id)
+    group_idp = dp$group_id
+  )
 
   list(stan_dat = sd, f = fp)
 }
@@ -87,11 +97,12 @@ prep_stan_dat <- function(d, predictors = "xscaled", arrange_by_group = TRUE) {
 
   # in case some missing now (e.g. cross-validation):
   d <- d %>% mutate(
-      group = as.factor(as.character(group)),
-      group_id = as.integer(group))
+    group = as.factor(as.character(group)),
+    group_id = as.integer(group)
+  )
 
   if (arrange_by_group) {
-   d <- d %>% arrange(group_id, x)
+    d <- d %>% arrange(group_id, x)
   }
   d1 <- filter(d, !is.na(y1))
   dp <- filter(d, !is.na(yp))
@@ -123,7 +134,8 @@ prep_stan_dat <- function(d, predictors = "xscaled", arrange_by_group = TRUE) {
     Ng = length(unique(as.character(d$group))),
     group_id0 = d$group_id,
     group_id1 = d1$group_id,
-    group_idp = dp$group_id)
+    group_idp = dp$group_id
+  )
 
   list(stan_dat = sd, f = fp)
 }
@@ -139,7 +151,7 @@ log_mean_exp <- function(u) {
 }
 
 fit_model <- function(d, model, predictors = "xscaled", iter = 800L, chains = 4L,
-  adapt_delta = 0.8, log_lik = TRUE, ...) {
+                      adapt_delta = 0.8, log_lik = TRUE, ...) {
   library(rstan)
 
   pars <- c("b0_j", "b1_j", "bp_j", "phi", "sigma_z", "z0_g", "z1_g", "zp_g")
@@ -155,7 +167,7 @@ fit_model <- function(d, model, predictors = "xscaled", iter = 800L, chains = 4L
 }
 
 fit_model_oib <- function(d, model, predictors = "xscaled", iter = 800L, chains = 4L,
-  adapt_delta = 0.8, log_lik = TRUE, ...) {
+                          adapt_delta = 0.8, log_lik = TRUE, ...) {
   library(rstan)
   pars <- c("b1_j", "bp_j", "phi", "sigma_z", "z1_g", "zp_g")
   if (log_lik) pars <- c(pars, "log_lik")
@@ -170,12 +182,12 @@ fit_model_oib <- function(d, model, predictors = "xscaled", iter = 800L, chains 
 }
 
 make_predictions_oib <- function(d, f, model, Npred = 200L, use_new_data = TRUE,
-  re = TRUE, sd_x = NULL, mean_x = NULL) {
-
+                                 re = TRUE, sd_x = NULL, mean_x = NULL) {
   if (use_new_data) {
     d_pred <- expand.grid(
       xscaled = seq(min(d$xscaled), max(d$xscaled), length.out = Npred),
-      group_id = unique(d$group_id), y = 1)
+      group_id = unique(d$group_id), y = 1
+    )
   } else {
     d_pred <- d
     Npred
@@ -194,22 +206,24 @@ make_predictions_oib <- function(d, f, model, Npred = 200L, use_new_data = TRUE,
     group = d_pred$group_id,
     est = apply(y_pred, 1, quantile, probs = 0.5),
     lwr = apply(y_pred, 1, quantile, probs = 0.05),
-    upr = apply(y_pred, 1, quantile, probs = 0.95))
+    upr = apply(y_pred, 1, quantile, probs = 0.95)
+  )
 
-  if (is.null(sd_x))
+  if (is.null(sd_x)) {
     pred_df$x <- d_pred$xscaled * 2 * unique(d$xraw_sd) + unique(d$xraw_mean)
+  }
 
   pred_df
 }
 
 
 make_predictions <- function(d, f, model, Npred = 200L, use_new_data = TRUE,
-  re = TRUE, sd_x = NULL, mean_x = NULL) {
-
+                             re = TRUE, sd_x = NULL, mean_x = NULL) {
   if (use_new_data) {
     d_pred <- expand.grid(
       xscaled = seq(min(d$xscaled), max(d$xscaled), length.out = Npred),
-      group_id = unique(d$group_id), y = 1)
+      group_id = unique(d$group_id), y = 1
+    )
   } else {
     d_pred <- d
   }
@@ -229,27 +243,14 @@ make_predictions <- function(d, f, model, Npred = 200L, use_new_data = TRUE,
     pred_p_re <- mm_pred %*% t(e$bp_j)
     pred_p_re <- apply(pred_p_re, 1, median)
     zp_g <- apply(e$zp_g, 2, median)
-    # zp_g <- tibble(zp_g = zp_g, group_id = seq_along(zp_g))
 
     pred_0_re <- mm_pred %*% t(e$b0_j)
     pred_0_re <- apply(pred_0_re, 1, median)
     z0_g <- apply(e$z0_g, 2, median)
-    # z0_g <- tibble(z0_g = z0_g, group_id = seq_along(z0_g))
 
     pred_1_re <- mm_pred %*% t(e$b1_j)
     pred_1_re <- apply(pred_1_re, 1, median)
     z1_g <- apply(e$z1_g, 2, median)
-    # z1_g <- tibble(z1_g = z1_g, group_id = seq_along(z1_g))
-    #
-    # d_pred <- inner_join(d_pred, zp_g, by = "group_id")
-    # d_pred <- inner_join(d_pred, z0_g, by = "group_id")
-    # d_pred <- inner_join(d_pred, z1_g, by = "group_id")
-    #
-    # d_pred <- mutate(d_pred,
-    #   pred_p_re = pred_p_re + zp_g,
-    #   pred_0_re = pred_0_re + z0_g,
-    #   pred_1_re = pred_1_re + z1_g
-    # )
 
     d_pred$pred_p_re <- plogis(pred_p_re + rep(zp_g, each = Npred))
     d_pred$pred_0_re <- plogis(pred_0_re + rep(z0_g, each = Npred))
@@ -263,21 +264,26 @@ make_predictions <- function(d, f, model, Npred = 200L, use_new_data = TRUE,
     group = d_pred$group_id,
     est = apply(y_pred, 1, quantile, probs = 0.5),
     lwr = apply(y_pred, 1, quantile, probs = 0.025),
-    upr = apply(y_pred, 1, quantile, probs = 0.975))
+    upr = apply(y_pred, 1, quantile, probs = 0.975)
+  )
 
-  if (re)
-    pred_df$est_re = d_pred$y_pred_re
+  if (re) {
+    pred_df$est_re <- d_pred$y_pred_re
+  }
 
-  if (is.null(sd_x))
+  if (is.null(sd_x)) {
     pred_df$x <- d_pred$xscaled * 2 * unique(d$xraw_sd) + unique(d$xraw_mean)
+  }
 
   pred_df
 }
 
 make_pred_plot <- function(pred_df, raw_data, re = TRUE) {
   g <- ggplot(pred_df, aes(x, est, ymin = lwr, ymax = upr)) +
-    geom_point(data = raw_data, aes(x, y, colour = group),
-      inherit.aes = FALSE, alpha = 0.6) +
+    geom_point(
+      data = raw_data, aes(x, y, colour = group),
+      inherit.aes = FALSE, alpha = 0.6
+    ) +
     geom_line(lwd = 1.5) +
     geom_ribbon(alpha = 0.5, fill = "grey20") +
     ylim(0, 1) +
@@ -285,9 +291,11 @@ make_pred_plot <- function(pred_df, raw_data, re = TRUE) {
     ylab("Proportion") +
     xlab("Predictor value") +
     labs(colour = "Fire location")
-  if (re)
+  if (re) {
     g <- g + geom_line(aes(y = est_re, group = as.factor(group)),
-      col = "grey35", lty = 1, lwd = 0.4, alpha = 0.95)
+      col = "grey35", lty = 1, lwd = 0.4, alpha = 0.95
+    )
+  }
   g
 }
 
@@ -323,7 +331,7 @@ make_predictions_dat <- function(model, d, f) {
 }
 
 make_roc <- function(d, predictions, return_plot = TRUE,
-  thresh = seq(0.1, 0.9, length.out = 20)) {
+                     thresh = seq(0.1, 0.9, length.out = 20)) {
   auc_thresh <- function(thresh = 0.5) {
     d$outcome <- ifelse(d$y > thresh, 1, 0)
     fastAUC(class = d$outcome, probs = predictions)
@@ -345,14 +353,14 @@ make_roc <- function(d, predictions, return_plot = TRUE,
     geom_text(data = data.frame(fpr = 0.75, tpr = 0.1), label = lab, col = "grey30") +
     geom_text(data = data.frame(fpr = 0.75, tpr = 0.15), label = lab1, col = "grey30") +
     ggsidekick::theme_sleek()
-  if (return_plot)
+  if (return_plot) {
     return(g)
-  else
+  } else {
     return(a)
+  }
 }
 
 plot_zoib_coefs <- function(obj, oib = FALSE, newnames_df = NULL, return_df = FALSE) {
-
   m <- obj$m
   x_name <- paste0(" ", obj$x) # " " for fct order
 
@@ -361,22 +369,28 @@ plot_zoib_coefs <- function(obj, oib = FALSE, newnames_df = NULL, return_df = FA
     num = as.character(seq_along(colnames(m$stan_dat$Xp_ij)))
   )
 
-  models <- tibble(model = c("0", "1", "p"),
+  models <- tibble(
+    model = c("0", "1", "p"),
     model_full = factor(
       c("Pr(not 0)", "Pr(1)", "Proportion"),
       levels =
         c("Pr(not 0)", "Proportion", "Pr(1)")
-    ))
-  if (oib)
-    models <- filter(models, model != 0) # oib vs zoib
+    )
+  )
+  if (oib) {
+    models <- filter(models, model != 0)
+  } # oib vs zoib
 
   b <- broom::tidyMCMC(m$model, estimate.method = "median", conf.int = TRUE)
-  b_inner <- broom::tidyMCMC(m$model, estimate.method = "median", conf.int = TRUE,
-    conf.level = 0.5) %>%
+  b_inner <- broom::tidyMCMC(m$model,
+    estimate.method = "median", conf.int = TRUE,
+    conf.level = 0.5
+  ) %>%
     rename(conf.low.25 = conf.low, conf.high.75 = conf.high) %>%
     select(-estimate, -std.error)
 
-  b <- b %>% inner_join(b_inner, by = "term") %>%
+  b <- b %>%
+    inner_join(b_inner, by = "term") %>%
     filter(grepl("b[01p]+_j", term)) %>%
     filter(!grepl("\\[1\\]", term)) %>%
     mutate(num = gsub("b[01p]+_j\\[([0-9]+)\\]", "\\1", term)) %>%
@@ -395,11 +409,11 @@ plot_zoib_coefs <- function(obj, oib = FALSE, newnames_df = NULL, return_df = FA
   # pal <- c(pal[4], "grey50", pal[5])
 
   ddat <- b %>%
-    mutate(estimate = ifelse(model == 0,     -estimate,     estimate)) %>%
-    mutate(conf.low = ifelse(model == 0,     -conf.low,     conf.low)) %>%
-    mutate(conf.high = ifelse(model == 0,    -conf.high,    conf.high)) %>%
+    mutate(estimate = ifelse(model == 0, -estimate, estimate)) %>%
+    mutate(conf.low = ifelse(model == 0, -conf.low, conf.low)) %>%
+    mutate(conf.high = ifelse(model == 0, -conf.high, conf.high)) %>%
     mutate(conf.high.75 = ifelse(model == 0, -conf.high.75, conf.high.75)) %>%
-    mutate(conf.low.25 = ifelse(model == 0,  -conf.low.25,  conf.low.25)) %>%
+    mutate(conf.low.25 = ifelse(model == 0, -conf.low.25, conf.low.25)) %>%
     mutate(interaction = grepl(":", name)) %>%
     inner_join(models, by = "model") %>%
     mutate(xvar = fct_relevel(name, x_name, after = Inf))
@@ -409,13 +423,17 @@ plot_zoib_coefs <- function(obj, oib = FALSE, newnames_df = NULL, return_df = FA
     return(ddat)
   }
 
-  ggplot(ddat, aes(y = estimate, x = xvar,
-      colour = model_full, shape = interaction)) +
+  ggplot(ddat, aes(
+    y = estimate, x = xvar,
+    colour = model_full, shape = interaction
+  )) +
     geom_hline(yintercept = 0, lty = 2, col = "grey55") +
     geom_linerange(aes(ymin = conf.low.25, ymax = conf.high.75),
-      position = position_dodge(width = 0.4), size = 1.2) +
+      position = position_dodge(width = 0.4), size = 1.2
+    ) +
     geom_pointrange(aes(ymin = conf.low, ymax = conf.high),
-      position = position_dodge(width = 0.4)) +
+      position = position_dodge(width = 0.4)
+    ) +
     xlab("") +
     ggsidekick::theme_sleek() +
     coord_flip() +
@@ -427,8 +445,7 @@ plot_zoib_coefs <- function(obj, oib = FALSE, newnames_df = NULL, return_df = FA
 }
 
 plot_interaction <- function(obj, int_var, int_lab, xlab, quant = c(0.1, 0.9),
-  cols = c("black", "red"), title = "", type = c("zoib", "oib"), return_data = FALSE) {
-
+                             cols = c("black", "red"), title = "", type = c("zoib", "oib"), return_data = FALSE) {
   type <- match.arg(type)
 
   newdata <- obj$d
@@ -436,12 +453,13 @@ plot_interaction <- function(obj, int_var, int_lab, xlab, quant = c(0.1, 0.9),
   cn <- cn[!grepl("\\(", cn)]
   cn <- cn[!grepl("\\:", cn)]
   cn <- cn[!grepl("xscaled", cn)]
-  newdata[,cn] <- 0
+  newdata[, cn] <- 0
   newdata[, "xscaled"] <- seq(min(newdata[, "xscaled"]),
     max(newdata[, "xscaled"]),
-    length.out = nrow(newdata))
+    length.out = nrow(newdata)
+  )
 
-  newdata[,int_var] <- quantile(obj$d[, int_var, drop = TRUE], quant[[1]])
+  newdata[, int_var] <- quantile(obj$d[, int_var, drop = TRUE], quant[[1]])
   newdata$level <- as.character(quant[[1]])
   newdata1 <- newdata
 
@@ -453,28 +471,36 @@ plot_interaction <- function(obj, int_var, int_lab, xlab, quant = c(0.1, 0.9),
   }
 
   if (type == "oib") {
-    p <- make_predictions_oib(d = newdata, f = obj$m$f,
-      model = obj$m$model, re = FALSE, use_new_data = FALSE)
+    p <- make_predictions_oib(
+      d = newdata, f = obj$m$f,
+      model = obj$m$model, re = FALSE, use_new_data = FALSE
+    )
   }
   if (type == "zoib") {
-    p <- make_predictions(d = newdata, f = obj$m$f,
-      model = obj$m$model, re = FALSE, use_new_data = FALSE)
+    p <- make_predictions(
+      d = newdata, f = obj$m$f,
+      model = obj$m$model, re = FALSE, use_new_data = FALSE
+    )
   }
 
   p$level <- newdata$level
   names(cols) <- quant
 
   if (!return_data) {
-    g <- ggplot(p, aes(x, est, ymin = lwr, ymax = upr, group = level,
-      fill = level)) +
+    g <- ggplot(p, aes(x, est,
+      ymin = lwr, ymax = upr, group = level,
+      fill = level
+    )) +
       geom_line(lwd = 1.5, aes(colour = level)) +
       geom_ribbon(alpha = 0.3) +
       ylim(0, 1) +
       ggsidekick::theme_sleek() +
       ylab("Proportion") +
       xlab(xlab) +
-      labs(colour = paste("Quantile", int_lab),
-        fill = paste("Quantile", int_lab)) +
+      labs(
+        colour = paste("Quantile", int_lab),
+        fill = paste("Quantile", int_lab)
+      ) +
       scale_colour_manual(values = cols, guide = guide_legend(reverse = TRUE)) +
       scale_fill_manual(values = cols, guide = guide_legend(reverse = TRUE)) +
       coord_cartesian(expand = FALSE) +
@@ -485,46 +511,3 @@ plot_interaction <- function(obj, int_var, int_lab, xlab, quant = c(0.1, 0.9),
     return(as_tibble(p))
   }
 }
-
-#
-# zoib_lpd <- function() {
-#   e <- rstan::extract(x$m$model)
-#   mm_pred <- cbind(rep(1, nrow(pred_dat)), pred_dat$xscaled)
-#   re_id <- x$d[x$d$group == "rr", "group_id", drop = TRUE][[1]]
-#   mu_inv_logit <- plogis(mm_pred %*% t(e$bp_j) +
-#     rep(e$zp_g[,re_id,drop=TRUE], each = nrow(mm_pred)))
-#   phi <- e$phi
-#   nmcmc <- ncol(mu_inv_logit)
-#   # # add obs. error:
-#   # pred_p <- t(apply(mu_inv_logit, 1, function(.x) {
-#   #   rbeta(n = nmcmc,
-#   #   shape1 = .x * phi,
-#   #   shape2 = (1 - .x) * phi)
-#   # }))
-#   pred_0 <- plogis(mm_pred %*% t(e$b0_j) +
-#       rep(e$z0_g[,re_id,drop=TRUE], each = nrow(mm_pred)))
-#   pred_1 <- plogis(mm_pred %*% t(e$b1_j) +
-#       rep(e$z1_g[,re_id,drop=TRUE], each = nrow(mm_pred)))
-#
-#   y_pred <- (1 - pred_0) * (pred_1 + (1 - pred_1) * pred_p)
-#
-#   pred_df <- data.frame(
-#     pp_est = apply(pred_p, 1, quantile, probs = 0.5),
-#     p0_est = apply(pred_0, 1, quantile, probs = 0.5),
-#     p1_est = apply(pred_1, 1, quantile, probs = 0.5),
-#     q0.05 = apply(y_pred, 1, quantile, probs = 0.05),
-#     q0.50 = apply(y_pred, 1, quantile, probs = 0.5),
-#     q0.95 = apply(y_pred, 1, quantile, probs = 0.95),
-#     sd     = apply(y_pred, 1, sd),
-#     mean   = apply(y_pred, 1, mean)
-#   )
-#   pred_df <- mutate(pred_df, ci_width_90 = q0.95 - q0.05)
-#
-#   stopifnot(nrow(pred_df) == nrow(pred_dat))
-#   pred_df$x <- pred_dat$x
-#   pred_df$y <- pred_dat$y
-#   pred_df$RdNBR_BL <- pred_dat$RdNBR_BL
-#   pred_df$response <- x$y
-#   pred_df$predictor <- x$x
-#   pred_df
-# })
